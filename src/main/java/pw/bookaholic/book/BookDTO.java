00fd644f20f56067479d3bf6c89a4d3a1302b7c6
Daria Komarynska <dashakomar777@gmail.com>
2023-05-01 15:56:53 +0200

    added dto, mapper

--- src/main/java/pw/bookaholic/book/BookDTO.java
+++ src/main/java/pw/bookaholic/book/BookDTO.java
@@ -1,4 +1,19 @@
 package pw.bookaholic.book;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Builder
+@Data
 public class BookDTO {
+    private UUID id;
+    @NotNull
+    private String title;
+    private String description;
+    private String cover;
+    private Integer pages;
+    private Long published;
 }
