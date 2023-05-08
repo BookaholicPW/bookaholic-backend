971608aeb6cc44bf3c64c407d1419fb8ef2345b4
Daria Komarynska <dashakomar777@gmail.com>
2023-05-08 16:36:33 +0200

    added genre model and crud, fixed ids

--- src/main/java/pw/bookaholic/book/Book.java
+++ src/main/java/pw/bookaholic/book/Book.java
@@ -3,8 +3,10 @@ package pw.bookaholic.book;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 import pw.bookaholic.author.Author;
 import pw.bookaholic.bookCharacter.BookCharacter;
+import pw.bookaholic.bookGenre.Genre;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -19,22 +21,24 @@ import java.util.UUID;
 @Setter
 public class Book {
     @Id
-    private UUID id; // = UUID.randomUUID();
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id = UUID.randomUUID();
     @NotNull
     private String title;
     private String description;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "book_author",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
-//    @ManyToMany
-//    @JoinTable(name = "book_genre",
-//            joinColumns = @JoinColumn(name = "book_id"),
-//            inverseJoinColumns = @JoinColumn(name = "genre_id"))
-//    private Set<Genre> genres = new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "book_genre",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private Set<Genre> genres = new HashSet<>();
 
 //    @OneToMany(mappedBy = "book")
 //    private Set<BookCharacter> characters = new HashSet<>();
