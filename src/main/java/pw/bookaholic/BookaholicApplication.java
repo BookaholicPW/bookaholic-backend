de8d537445b15318562451a6d34dedcde730b060
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-05-16 22:14:11 +0200

    Fix swagger server (#37)

--- src/main/java/pw/bookaholic/BookaholicApplication.java
+++ src/main/java/pw/bookaholic/BookaholicApplication.java
@@ -1,10 +1,17 @@
 package pw.bookaholic;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
+)
 @SpringBootApplication
 @SecurityScheme(
         name = "Bearer Authentication",
