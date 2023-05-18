7c5feaab5443cddc6cbef472b7d1bbd5ef91571f
mm-owicz <01158833@pw.edu.pl>
2023-05-19 00:34:36 +0200

    import fixes

--- src/main/java/pw/bookaholic/bookCharacter/BookCharacter.java
+++ src/main/java/pw/bookaholic/bookCharacter/BookCharacter.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import pw.bookaholic.book.Book;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.UUID;
 
@@ -24,9 +26,7 @@ public class BookCharacter {
     private String name;
 
     @ManyToOne
-    @JoinColumn(
-            nullable = false,
-            name= "book_id"
-    )
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "book_id")
     private Book book;
 }
