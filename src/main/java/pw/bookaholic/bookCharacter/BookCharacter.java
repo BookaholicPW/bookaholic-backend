995d95b448e8a5cd6516854045de7a328df6eac6
Daria Komarynska <dashakomar777@gmail.com>
2023-05-21 23:41:44 +0200

    fixed post enpdoint, added character api

--- src/main/java/pw/bookaholic/bookCharacter/BookCharacter.java
+++ src/main/java/pw/bookaholic/bookCharacter/BookCharacter.java
@@ -3,8 +3,10 @@ package pw.bookaholic.bookCharacter;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.type.SqlTypes;
 import pw.bookaholic.book.Book;
 
 import java.util.UUID;
@@ -21,6 +23,8 @@ public class BookCharacter {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(length = 16, columnDefinition = "binary(16)", updatable = false, nullable = false)
     private UUID id;
 
     private String name;
