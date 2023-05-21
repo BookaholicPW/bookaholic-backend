547c262709e6e1efe660381c0018a2259fa4394e
Daria Komarynska <87605159+DariaKomarynska@users.noreply.github.com>
2023-05-21 21:24:37 +0200

    added data with liquibase (#72)

---
+++ src/main/resources/db/DDL_generated/drop_tables.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `databasechangeloglock`;
+DROP TABLE IF EXISTS `databasechangelog`;
+DROP TABLE IF EXISTS `book_genre`;
+DROP TABLE IF EXISTS `book_character`;
+DROP TABLE IF EXISTS `genre`;
+DROP TABLE IF EXISTS `book`;
+DROP TABLE IF EXISTS `author`;
+DROP TABLE IF EXISTS `users`;
