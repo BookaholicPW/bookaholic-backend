0e86575d5328ec6320ba11a57d53e9f3482da048
mm-owicz <01158833@pw.edu.pl>
2023-05-21 22:08:45 +0200

    chat message service methods

--- src/main/java/pw/bookaholic/chatMessage/ChatMessageRepository.java
+++ src/main/java/pw/bookaholic/chatMessage/ChatMessageRepository.java
@@ -2,7 +2,10 @@ package pw.bookaholic.chatMessage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pw.bookaholic.book.Book;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -10,4 +13,7 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> 
 
     ChatMessage findBySender(UUID senderId);
 
+    Optional<ChatMessage> findById(UUID id);
+
+    List<ChatMessage> findAllBySender(UUID id);
 }
--- src/main/java/pw/bookaholic/chatMessage/ChatMessageService.java
+++ src/main/java/pw/bookaholic/chatMessage/ChatMessageService.java
@@ -1,7 +1,17 @@
 package pw.bookaholic.chatMessage;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Service;
+import pw.bookaholic.user.User;
+
+import java.util.Date;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static pw.bookaholic.utils.Utils.response;
 
 @Service
 @AllArgsConstructor
@@ -11,6 +21,20 @@ public class ChatMessageService {
 
     private ChatMessageMapper mapper;
 
+    public Date getChatMessageTime(User user){
+        ChatMessage message = repository.findBySender(user.getId());
+        return message.getTime();
+    }
+
+    public Date getChatMessageTime(UUID id){
+        ChatMessage message = repository.findById(id).orElseThrow(() -> new NoSuchElementException("No such message."));
+        return message.getTime();
+    }
+
+    public Object getAllMessagesBySender(User user){
+        return response(repository.findAllBySender(user.getId()).stream().map(mapper::chatMessageToChatMessageDto).collect(Collectors.toList()),
+                        "Successfully found all messages from sender.");
+    }
 
 
 }
