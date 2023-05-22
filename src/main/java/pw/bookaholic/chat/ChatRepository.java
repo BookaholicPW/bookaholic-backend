72debaf13c621a185ebd60600441d9062c3549d9
Truong Giang Do <giangh14cqt@gmail.com>
2023-05-22 13:57:07 +0200

    Add chat query

--- src/main/java/pw/bookaholic/chat/ChatRepository.java
+++ src/main/java/pw/bookaholic/chat/ChatRepository.java
@@ -1,6 +1,7 @@
 package pw.bookaholic.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,5 +9,12 @@ import java.util.UUID;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
+    @Query(
+            value = """
+                    select * from chat
+                    where first_user_id = ?1 or second_user_id = ?1
+                    """,
+            nativeQuery = true
+    )
     List<Chat> findAllByFirstUserOrSecondUser(UUID id);
 }
