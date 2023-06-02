691e79dd093cd5779bdacf2e1f6224d57ea23b7d
Daria Komarynska <87605159+DariaKomarynska@users.noreply.github.com>
2023-06-02 21:46:18 +0200

    user data (#80)
    * user data
    
    * updated create table

--- src/main/resources/db/DDL_generated/create_tables.sql
+++ src/main/resources/db/DDL_generated/create_tables.sql
@@ -46,8 +46,7 @@ CREATE TABLE `book` (
                         `title` varchar(255) NOT NULL,
                         `author_id` binary(16) DEFAULT NULL,
                         PRIMARY KEY (`id`),
-                        KEY `FKsytpuhl67g1174cx7iqlw6os9` (`author_id`),
-                        CONSTRAINT `FKsytpuhl67g1174cx7iqlw6os9` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE
+                        CONSTRAINT FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -68,9 +67,8 @@ CREATE TABLE `book_genre` (
                               `book_id` binary(16) NOT NULL,
                               `genre_id` binary(16) NOT NULL,
                               PRIMARY KEY (`book_id`,`genre_id`),
-                              KEY `FKt8we15a66nkehlgncvl8qm1dy` (`genre_id`),
-                              CONSTRAINT `FKclmlyua3d5iu4ryf24x4g2qao` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
-                              CONSTRAINT `FKt8we15a66nkehlgncvl8qm1dy` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
+                              CONSTRAINT FOREIGN KEY (`book_id`) REFERENCES `book` (`id`),
+                              CONSTRAINT FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -83,7 +81,43 @@ CREATE TABLE `book_character` (
                                   `name` varchar(255) DEFAULT NULL,
                                   `book_id` binary(16) DEFAULT NULL,
                                   PRIMARY KEY (`id`),
-                                  KEY `FK7jca5t2n88nlu67opt8gm7e7g` (`book_id`),
-                                  CONSTRAINT `FK7jca5t2n88nlu67opt8gm7e7g` FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE
+                                  CONSTRAINT FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
+CREATE TABLE `matching` (
+                            `id` binary(16) NOT NULL,
+                            `first_user_liked` bit(1) DEFAULT NULL,
+                            `second_user_liked` bit(1) DEFAULT NULL,
+                            `first_user_id` binary(16) DEFAULT NULL,
+                            `second_user_id` binary(16) DEFAULT NULL,
+                            PRIMARY KEY (`id`),
+                            CONSTRAINT FOREIGN KEY (`first_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+                            CONSTRAINT FOREIGN KEY (`second_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+CREATE TABLE `users_authors` (
+                                 `user_id` binary(16) NOT NULL,
+                                 `authors_id` binary(16) NOT NULL,
+                                 CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+                                 CONSTRAINT FOREIGN KEY (`authors_id`) REFERENCES `author` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `users_books` (
+                               `user_id` binary(16) NOT NULL,
+                               `books_id` binary(16) NOT NULL,
+                               CONSTRAINT FOREIGN KEY (`books_id`) REFERENCES `book` (`id`),
+                               CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `users_genres` (
+                                `user_id` binary(16) NOT NULL,
+                                `genres_id` binary(16) NOT NULL,
+                                CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+                                CONSTRAINT FOREIGN KEY (`genres_id`) REFERENCES `genre` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/main/resources/db/changelog/create_table/Create-schema.1.0.sql
+++ src/main/resources/db/changelog/create_table/Create-schema.1.0.sql
@@ -2,6 +2,26 @@
 
 --changeset Daria:1
 
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE IF NOT EXISTS `users` (
+                         `id` binary(16) NOT NULL,
+                         `avatar` varchar(255) DEFAULT NULL,
+                         `bio` varchar(255) DEFAULT NULL,
+                         `created_at` bigint DEFAULT NULL,
+                         `email` varchar(255) NOT NULL,
+                         `name` varchar(255) DEFAULT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `updated_at` bigint DEFAULT NULL,
+                         `username_` varchar(255) DEFAULT NULL,
+                         `verified` bit(1) DEFAULT 0 NOT NULL,
+                          UNIQUE KEY (`username_`),
+                         PRIMARY KEY (`id`)
+);
+
+
 --
 -- Table structure for table `author`
 --
@@ -59,3 +79,41 @@ CREATE TABLE IF NOT EXISTS `book_character` (
                                   PRIMARY KEY (`id`),
                                    FOREIGN KEY (`book_id`) REFERENCES `book` (`id`) ON DELETE CASCADE
 ) ;
+
+
+
+CREATE TABLE IF NOT EXISTS `users_authors` (
+    `user_id` binary(16) NOT NULL,
+    `authors_id` binary(16) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    FOREIGN KEY (`authors_id`) REFERENCES `author` (`id`)
+    ) ;
+
+
+CREATE TABLE IF NOT EXISTS `users_books` (
+    `user_id` binary(16) NOT NULL,
+    `books_id` binary(16) NOT NULL,
+    FOREIGN KEY (`books_id`) REFERENCES `book` (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    ) ;
+
+
+CREATE TABLE IF NOT EXISTS `users_genres` (
+    `user_id` binary(16) NOT NULL,
+    `genres_id` binary(16) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    FOREIGN KEY (`genres_id`) REFERENCES `genre` (`id`)
+    ) ;
+
+
+
+CREATE TABLE IF NOT EXISTS `matching` (
+    `id` binary(16) NOT NULL,
+    `first_user_liked` bit(1) DEFAULT NULL,
+    `second_user_liked` bit(1) DEFAULT NULL,
+    `first_user_id` binary(16) DEFAULT NULL,
+    `second_user_id` binary(16) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`first_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`second_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+    ) ;
