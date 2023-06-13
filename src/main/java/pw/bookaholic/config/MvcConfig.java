5ee77215f940bfd3199d0af92d18969772b5da91
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-06-14 01:59:39 +0200

    Add avt upload/download (#84)

---
+++ src/main/java/pw/bookaholic/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package pw.bookaholic.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/content/**")
+                .addResourceLocations("file:" + System.getenv("CONTENT_LOCATION"));
+    }
+}
